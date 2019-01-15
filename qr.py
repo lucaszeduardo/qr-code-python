597f07093e649ba7652b99165558c65804810ccc
bernardo freitas <bernardofreitas_2011@hotmail.com>
2019-01-14 22:02:49 -0200

    refatorado

--- qr.py
+++ qr.py
@@ -1,12 +1,14 @@
 import pyqrcode
 from hashlib import md5
-from os import system, name  # noqa
+from os import system as sy, name  # noqa
+from PIL import Image
+from pyzbar.pyzbar import decode
 
 if name == 'posix':
     limpar = 'clear'
 else:
     limpar = 'cls'
-system(limpar)
+sy(limpar)
 
 
 def qrcode(hash, name):
@@ -15,17 +17,30 @@ def qrcode(hash, name):
     print('QrCode Generated....')
 
 
-txt = input('Enter your text: ')
-name = input('Enter name for file.png: ')
-option = input('do you want to encrypt the message? [y/n]: ').upper()
+def main():
+    create = input('Do you want create or load a qr file [c/l]: ').lower()
+    if create == 'c':
+        txt = input('Enter your text: ')
+        name = input('Enter name for file.png: ')
+        option = input('do you want to encrypt the message? [y/n]: ').lower()
+        if name[-4:] != '.png':
+            name += '.png'
+        if option == 'Y':
+            hash = md5(txt.encode()).hexdigest()
+            qrcode(hash, name)
+        elif option == 'N':
+            qrcode(txt, name)
+        else:
+            print('Invalid option.')
+    elif create == 'l':
+        name = input('Enter name file.png: ')
+        data = str(decode(Image.open(name))[0][0])
+        if input('This file is cript? [y/n]: ').upper() == 'Y':
+            response = data.replace("'", '')[1:]
+        else:
+            response = data.replace('b', '').replace('\'', '')
+        print(response)
 
-if name[-4:] != '.png':
-    name += '.png'
 
-if option == 'Y':
-    hash = md5(txt.encode()).hexdigest()
-    qrcode(hash, name)
-elif option == 'N':
-    qrcode(txt, name)
-else:
-    print('Invalid option.')
+if __name__ == '__main__':
+    main()
